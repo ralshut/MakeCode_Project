45156f30f73c9ee91ac9f0f7d5ce9e56ba46329c
ralshut <153237280+ralshut@users.noreply.github.com>
2025-01-05 23:04:19 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,6 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    basic.showIcon(IconNames.Happy)
+    basic.pause(500)
+    basic.showIcon(IconNames.ArrowNorthWest)
+    basic.pause(500)
+})
